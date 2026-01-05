da8da28a82ef3f226736398e22239c6360f79721
guohongjun <guohongjun1128@163.com>
2026-01-05 11:34:25 +0800

    测试大模型连通性-2

--- api/[...all].ts
+++ api/[...all].ts
@@ -29,7 +29,7 @@ import finish from './_src/server/routes/finish.js'
 import report from './_src/server/routes/report.js'
 import metrics from './_src/server/routes/metrics.js'
 import parseResumeTask from './_src/server/routes/parse-resume-task.js';
-import testLlm from './_src/server/routes/test-llm.ts';
+import testLlm from './_src/server/routes/test-llm.js';
 
 const app = new Hono()
 
